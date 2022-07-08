949c2c08c11ea9a96b193d7d766f2c8eeb1dfc51
Lukasz Halicki <uhalicki@gmail.com>
2022-07-08 14:50:07 +0200

    Resolved issue with reciving a single product from the server - Server and Client

--- BlazorEcommerceExample/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerceExample/Client/Services/ProductService/ProductService.cs
@@ -10,11 +10,18 @@
         }
         public List<Product> Products { get; set; } = new List<Product>();
 
+
         public async Task GetProducts()
         {
             var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
             if (result != null && result.Data != null)
                 Products = result.Data;
+        }
+
+        public async Task<ServiceResponse<Product>> GetProduct(int prouctId)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{prouctId}");
+            return result;
         }
     }
 }
